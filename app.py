e020cae107584796c0af708d8de9e3ebba4ee374
pakotoes3@gmail.com <paco@Paco>
2019-06-12 14:06:41 +0200

    cambios finales

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def filtro():
 def clash():
 	return render_template('clash.html')
 
+
 @app.route('/clash/jugador',methods=['GET','POST'])
 def tag():
 	h={"Accept":"application/json","authorization":"Bearer %s"%key}
@@ -52,11 +53,52 @@ def tag():
 	r=requests.get(URL_BASE2+"players/"+datos2,headers=h)
 	if r.status_code==200:
 		doc=r.json()
+		diccarta={}
 		listamazo=[]
 		for i in doc["currentDeck"]:
-			listamazo.append(i["iconUrls"]["medium"])
+			diccarta['imagen']=i["iconUrls"]["medium"]
+			diccarta['nombre']=i["name"]
+			listamazo.append(diccarta.copy())
 		return render_template("tag.html",doc=doc,listamazo=listamazo)
 	else:
 		abort(404)
+
+@app.route('/clash/clan',methods=['GET','POST'])
+def clan():
+	h={"Accept":"application/json","authorization":"Bearer %s"%key}
+	datos=request.form.get("Clan")
+	payload={"name": datos}
+	r=requests.get(URL_BASE2+"clans",headers=h,params=payload)
+	if r.status_code==200:
+		doc=r.json()
+		listaclanes=[]
+		for i in doc["items"]:
+			if i["members"] > 30:
+				listaclanes.append(i)
+		return render_template("clan.html",listaclanes=listaclanes)
+	else:
+		abort(404)
+
+@app.route('/clash/ranking',methods=['GET','POST'])
+def rank():
+	h={"Accept":"application/json","authorization":"Bearer %s"%key}
+	datos=request.form.get("Ranking")
+	r=requests.get(URL_BASE2+"locations",headers=h)
+	if r.status_code==200:
+		doc=r.json()
+		for i in doc["items"]:
+			if i["name"]==datos:
+				idloc=i["id"]
+				idloc=str(idloc)
+				payload={"limit": 25}
+				r2=requests.get(URL_BASE2+"locations/"+idloc+"/rankings/players",headers=h,params=payload)
+				if r2.status_code==200:
+					doc2=r2.json()
+					return render_template("rank.html",doc2=doc2,pais=datos)
+					break
+				else:
+					abort(404)
+	else:
+		abort(404)	
 
 app.run('0.0.0.0',debug=True)
